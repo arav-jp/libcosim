fbd53ec420762108e75a1162f2d65ee176505a30
tamago117 <taikibannsei2020@gmail.com>
2022-10-04 18:37:10 +0900

    fix cmake

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from os import path
 
 class LibcosimConan(ConanFile):
     name = "libcosim"
-    author = "osp"
+    author = "arav"
     exports = "version.txt"
     scm = {
         "type": "git",
@@ -53,7 +53,7 @@ class LibcosimConan(ConanFile):
 
     def requirements(self):
         if self.options.proxyfmu:
-            self.requires("proxyfmu/0.2.7@osp/stable")
+            self.requires("proxyfmu/0.2.9@osp/stable")
 
     def imports(self):
         binDir = os.path.join("output", str(self.settings.build_type).lower(), "bin")
